35162508e0ee9ee0ca7d43b8f4f8583d471a6ee2
Mario Cortes <macortes96272@misena.edu.co>
2020-05-30 11:12:32 -0500

    se carga crud de auditoria

--- code/controllers/admin/auditoria/cargarAuditoria.php
+++ code/controllers/admin/auditoria/cargarAuditoria.php
@@ -37,43 +37,23 @@ if (isset($_GET['id_auditoria'])){
                  id="puntaje"  >
             </div>
              </div>
-             
-
-
+             <div class="form-row">
             <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6 form-group">
             <label>Identificacion Empresa</label>
+            <input type="text" class="form-control" value="'.$f['id_empresa'].'" name="id_empresa" readonly="readonly
+                 id="id_empresa"  >
             
-            <select class="selectpicker" name="id_empresa" id="id_empresa"
-                required>
-                <option value="">--Seleccione--</option>
-            ';     
-             
-            foreach($empresas as $item)
-            {
-                echo'<option value="'.$item['id_empresa'].'">'.$item['nombre_empresa'].'</option>';
-                
-            }
-            echo'
-        
-             </select> 
             </div>
-            
-            
-         
-         <div class="form-row">
             <div class="col-md-6 col-sm-12 col-xs-12 col-lg-6 form-group">
             <label>Status </label>
-            <select class="selectpicker" name="status" id="status" value="'.$f['status'].'"
-                         required>
-                        <option >--Seleccione--</option>
-                        <option >Proceso</option>
-                        <option >Finalizado</option>  
+            <input type="text" class="form-control" value="'.$f['status'].'" name="id_empresa" readonly="readonly
+                 id="id_empresa"  >
+            
                             
 
                     </select> 
             </div>
-                </div>
-                </div>
+        </div>
         <div class="text-center">
                  <button type="submit"
                 class="btn btn-success loginbtn">Actualizar</button>
---
+++ code/controllers/admin/auditoria/eliminar.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('../../../models/conexion.php');
+require_once('../../../models/admin/auditoria/consultaAudito.php');
+
+if (isset($_GET['id_auditoriaE'])){
+    $idEliminar=$_GET['id_auditoriaE'];
+    $consultas=new consultaAuditoria();
+    $result=$consultas->eliminarAuditoria($idEliminar);
+
+}else{
+    echo'<script>alert("Error al eliminar la Auditoria")</script>';
+}
+?>
