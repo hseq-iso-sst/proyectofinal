564e9ea208d996ef541c5a443a71b55ec6217998
Mario Cortes <macortes96272@misena.edu.co>
2020-06-17 17:39:29 -0500

    se ajusta modulo de velaidar perfil para que muestre nombre y apellidos

--- code/controllers/validarperfil.php
+++ code/controllers/validarperfil.php
@@ -9,7 +9,7 @@
 
        foreach ($result as $f) {
            echo '<span class="ml-2 d-none d-lg-inline-block"><span>hola</span> <span
-           class="text-dark">'.$f['nombres_user'].'
+           class="text-dark">'.$f['nombres_user'].'  '.$f['apellidos_user'].'
            </span> <i data-feather="chevron-down" class="svg-icon"></i></span>
            ';
        }
