669f903df004a83ce367957f7218622ffebc3687
Mario Cortes <macortes96272@misena.edu.co>
2020-06-17 17:22:09 -0500

    se ajusta login para el llamdo del nombre

--- code/controllers/validarperfil.php
+++ code/controllers/validarperfil.php
@@ -1,17 +1,17 @@
-<!-- 
+<?php
 
-//  function perfil(){
+    function perfil(){
 
-//     $email_user= $_SESSION ['email_user'];
-//     $consultas= new consultasUsuario();
+       $email_user= $_SESSION ['email_user'];
+       $consultas= new Consultas();
 
-//     $result = $consultas ->validarperfil($email_user);
+       $result = $consultas ->validarperfil($email_user);
 
-//     foreach ($result as $f) {
-//         echo '<span class="ml-2 d-none d-lg-inline-block"><span>hola</span> <span
-//         class="text-dark">'.$f['nombres_user'].'
-//         </span> <i data-feather="chevron-down" class="svg-icon"></i></span>
-//         ';
-//     }
-//  }
-//?> -->
+       foreach ($result as $f) {
+           echo '<span class="ml-2 d-none d-lg-inline-block"><span>hola</span> <span
+           class="text-dark">'.$f['nombres_user'].'
+           </span> <i data-feather="chevron-down" class="svg-icon"></i></span>
+           ';
+       }
+    }
+?>
