c5ca1580e09281bdbfb471cc56be6a8c98887aac
Camilo chavez <juliansito-27@hotmail.com>
2020-07-04 13:50:42 -0500

    otro intento

--- code/models/admin/visita/consultasEvaluacion.php
+++ code/models/admin/visita/consultasEvaluacion.php
@@ -2,11 +2,7 @@
 
 class Consultas{
 
-<<<<<<< HEAD
-    public function insertarEvaluacion($id_visita, $id_requisito, $evaluacion, $comentario,){     
-=======
     public function insertarEvaluacion($id_visita, $id_requisito, $evaluacion, $comentario){     
->>>>>>> 1e0a15190dbadd0359cf57731cfeb64ff310df62
             $modelo = new Conexion();
                 $conexion = $modelo->get_conexion();
         
