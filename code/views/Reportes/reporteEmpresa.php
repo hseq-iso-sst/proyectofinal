c28531d658ecda003e10c5b1469c491c82691193
Mario Cortes <macortes96272@misena.edu.co>
2020-07-16 08:01:11 -0500

    ajuest de footer reportes

--- code/views/Reportes/reporteEmpresa.php
+++ code/views/Reportes/reporteEmpresa.php
@@ -20,6 +20,25 @@ require_once("../controllers/admin/empresa/cargarEmpresa1.php");
                 cargarEmpresa();
               ?>
 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer class="footer text-center text-muted">
+        <h6>Contactenos</h6>
+        <a href="https://www.facebook.com/" class="icon-social-facebook"> Facebook</a>
+        <a href="https://github.com/" class="icon-social-github">GitHub</a>
+        <a href="https://www.linkedin.com/" class="icon-social-linkedin">Linkedin</a>
+        <a href="https://twitter.com/" class="icon-social-twitter">Twitter</a>
+    </footer>
+
+
 
 
 
--- code/views/Reportes/reporteFicha.php
+++ code/views/Reportes/reporteFicha.php
@@ -19,6 +19,25 @@ require_once("../controllers/admin/ficha/cargarFicha1.php");
           <?php
                 cargarFicha();
               ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer class="footer text-center text-muted">
+        <h6>Contactenos</h6>
+        <a href="https://www.facebook.com/" class="icon-social-facebook"> Facebook</a>
+        <a href="https://github.com/" class="icon-social-github">GitHub</a>
+        <a href="https://www.linkedin.com/" class="icon-social-linkedin">Linkedin</a>
+        <a href="https://twitter.com/" class="icon-social-twitter">Twitter</a>
+    </footer>
+
+
 
 
 
